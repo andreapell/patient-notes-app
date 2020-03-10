142dc901f2328d9a78d0b45b7bcde86c3d0b54e0
andreapell <andreadepell@gmail.com>
2020-03-10 10:08:21 -0700

    adding notes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,6 @@
 class Patient < ApplicationRecord
+  has_many :notes
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
 end
